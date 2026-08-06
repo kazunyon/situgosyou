50b9cac0acb20dffc4d7ade55aedbd0cdc05f42c
kazunyon <your-email@example.com>
2026-08-07 08:02:19 +0900

    Update app icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,8 @@ export default defineConfig(({ command }) => ({
         display: 'standalone',
         lang: 'ja',
         icons: [
-          { src: 'pwa-192x192.svg', sizes: '192x192', type: 'image/svg+xml', purpose: 'any' },
-          { src: 'pwa-512x512.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'any' }
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' }
         ]
       }
     })
